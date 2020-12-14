da90d4f1b10c95b44bb391d5802c71b914917f28
dominikn <dominik.nwk@gmail.com>
2020-12-14 14:19:39 +0100

    basic async ota with husarnet working

---
+++ src/simple-webserver.ino
@@ -0,0 +1,60 @@
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
+#include <Husarnet.h>
+
+#if __has_include("credentials.h")
+#include "credentials.h"
+#else
+
+// WiFi credentials
+const char *ssid = "MY_WIFI_SSID";
+const char *password = "MY_WIFI_PASS";
+
+// Husarnet credentials
+const char *hostName = "ota-test";
+const char *husarnetJoinCode = "fc94:b01d:1803:8dd8:b293:5c7d:7639:932a/xxxxxxxxxxxxxxxxxxxxxxx"; // find at app.husarnet.com
+const char *dashboardURL = "default";
+
+#endif
+
+AsyncWebServer server(3232);
+
+void setup(void)
+{
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Start Husarnet
+  Husarnet.selfHostedSetup(dashboardURL);
+  Husarnet.join(husarnetJoinCode, hostName);
+  Husarnet.start();
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Hi! I am ESP32.");
+  });
+
+  AsyncElegantOTA.begin(&server); // Start ElegantOTA
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void)
+{
+  AsyncElegantOTA.loop();
+}

b47a6c3df667522d695dc2fcd97249af840ecd79
dominikn <dominik.nwk@gmail.com>
2021-09-17 14:44:10 +0200

    testing ga

--- .github/actions/husarnet-js/index.js
+++ .github/actions/husarnet-js/index.js
@@ -1,24 +1,24 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const exec = require('@actions/exec');
+const exec = require("@actions/exec");
 
-(async () => {
+
+
+// most @actions toolkit packages have async methods
+async function run() {
   try {
-    await exec.exec('curl https://install.husarnet.com/install.sh | sudo bash');
-  } catch (e) {
-      // Deal with the fact the chain failed
+    await exec.exec("curl https://install.husarnet.com/install.sh | sudo bash");
+    
+    const joincode = core.getInput("join-code");
+    console.log(`JoinCode of this GA: ${joincode}`);
+
+    const ipv6 = "fc94:2283:b56b:beeb:xxxx:xxxx:xxxx:xxxx";
+    core.setOutput("ipv6", ipv6);
+
+    console.log(JSON.stringify(github, null, "\t"));
+  } catch (error) {
+    core.setFailed(error.message);
   }
-})();
-
-try {
-  
-  const joincode = core.getInput("join-code");
-  console.log(`JoinCode of this GA: ${joincode}`);
-
-  const ipv6 = "fc94:2283:b56b:beeb:xxxx:xxxx:xxxx:xxxx";
-  core.setOutput("ipv6", ipv6);
-
-  console.log(JSON.stringify(github, null, "\t"));
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run();
